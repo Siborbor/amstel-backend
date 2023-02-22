8b42cd316c0390b7c8f3bfbaa34fa50b8fb403e1
Siborbor <89548886+Siborbor@users.noreply.github.com>
2023-02-22 14:41:00 -0500

    coors update

--- server.js
+++ server.js
@@ -1,41 +1,38 @@
-const express = require('express');
-const mysql = require('mysql2');
-const myconn = require('express-myconnection');
-const cors = require('cors')
+const express = require("express");
+const mysql = require("mysql2");
+const myconn = require("express-myconnection");
+const cors = require("cors");
 
-const routes = require('./routes')
-const config = require('./config');
+const routes = require("./routes");
 
 const app = express();
 //set el puerto de escucha
-app.use(cors(
-    config.application.cors.server
-));
+app.use(cors({ origin: "*" }));
 
-app.set('port', process.env.PORT || 9000);
+app.set("port", process.env.PORT || 9000);
 
 const dbOptions = {
-    host:"containers-us-west-141.railway.app",
-    port:6360,
-    user:'root',
-    password:"ZKmfreSSTTbQ968FjpI4",
-    database:'railway'
-}
+  host: "containers-us-west-141.railway.app",
+  port: 6360,
+  user: "root",
+  password: "ZKmfreSSTTbQ968FjpI4",
+  database: "railway",
+};
 
 //middlewares-----
-app.use(myconn(mysql,dbOptions, 'single'))
+app.use(myconn(mysql, dbOptions, "single"));
 app.use(express.json());
 
 //agregamos la ruta principal de nuestra api
 //router-----------------------------
-app.get('/',(req,res)=>{
-    res.send('welcome to my api');
-})
+app.get("/", (req, res) => {
+  res.send("welcome to my api");
+});
 
-app.use('/api',routes);
+app.use("/api", routes);
 
 //escucha el servidor
 //server running--------------------
-app.listen(app.get('port'), ()=>{
-    console.log('server running on port:', app.get('port') );
+app.listen(app.get("port"), () => {
+  console.log("server running on port:", app.get("port"));
 });
